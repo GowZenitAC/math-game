87ae03fa1c0bec638e23b81fbd7a4a3aced193c7
noecan <noecanyam8@gmail.com>
2024-02-07 17:42:48 -0600

    boton y sect personalizado

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -4,12 +4,16 @@
         <section>
             <p class="subtitle">Porfavor Selecciona tu equipo</p>
             <div class="select-teams">
-    <select name="team" id="1" @change="obtenerEquipos">
-      <option value="" disabled selected>Selecciona tu equipo</option>
-      <option v-for="equipo in equipos" :key="equipo.id" :value="equipo.id">{{ equipo.nombre }}</option>
-    </select>
-    <button @click="next">Seleccionar</button>
-  </div>
+                <div class="select-container">
+                    <select name="team" id="1" @change="obtenerEquipos">
+                        <option value="" disabled selected>Selecciona tu equipo</option>
+                        <option v-for="equipo in equipos" :key="equipo.id" :value="equipo.id">{{ equipo.nombre }}</option>
+                    </select>
+                </div>
+                <div class="botom-container">
+                    <button @click="next">Seleccionar</button>
+                </div>
+            </div>
         </section>
     </main>
 </template>
@@ -19,29 +23,29 @@ import axios from 'axios';
 let EQUIPOS_URL = 'http://127.0.0.1:8000/api/apiequipos';
 
 export default {
-  data() {
-    return {
-      equipos: [],
-    };
-  },
-  methods: {
-    next() {
-      this.$router.push({ name: 'game' });
+    data() {
+        return {
+            equipos: [],
+        };
     },
-    async obtenerEquipos() {
-    try {
-        const response = await axios.get(EQUIPOS_URL);
-        this.equipos = response.data;
-        console.log(this.equipos);
-    } catch (error) {
-        throw error;
-    }
-}
+    methods: {
+        next() {
+            this.$router.push({ name: 'game' });
+        },
+        async obtenerEquipos() {
+            try {
+                const response = await axios.get(EQUIPOS_URL);
+                this.equipos = response.data;
+                console.log(this.equipos);
+            } catch (error) {
+                throw error;
+            }
+        }
 
-  },
-  created() {
-    this.obtenerEquipos(); // Corregido el nombre del método
-  },
+    },
+    created() {
+        this.obtenerEquipos(); // Corregido el nombre del método
+    },
 };
 
 </script>
@@ -52,8 +56,8 @@ export default {
     width: 100%;
     height: 65dvh;
     box-shadow: inset 0 0 1em -0.5em #f6f6f6;
-    border-radius:15px;
-    
+    border-radius: 15px;
+
 }
 
 .title {
@@ -62,7 +66,8 @@ export default {
     font-size: 3rem;
     margin: 0 3rem 0 3rem;
 }
-.subtitle{
+
+.subtitle {
     text-align: center;
     margin-top: 1em;
     color: white;
@@ -70,22 +75,27 @@ export default {
     animation: shake-bottom 0.8s cubic-bezier(0.455, 0.030, 0.515, 0.955) both infinite;
 
 }
-.select-teams{
+
+.select-teams {
     display: flex;
-    justify-content: center;
-    margin-top: 1rem;
+    justify-content: space-between;
+
+    align-items: center;
 }
 
 /* Estilo del select */
 select {
     padding: 10px;
     font-size: 16px;
-    border: 2px solid #2980b9; /* Color del borde */
+    border: 2px solid #2980b9;
+    /* Color del borde */
     border-radius: 5px;
     outline: none;
     transition: border-color 0.3s ease;
-    width: 200px; /* Ajusta el ancho según tus necesidades */
-    cursor: pointer; /* Cambia el cursor al pasar sobre el select */
+    width: 200px;
+    /* Ajusta el ancho según tus necesidades */
+    cursor: pointer;
+    /* Cambia el cursor al pasar sobre el select */
     color: #f5f0f0;
     background-color: rgb(77, 71, 245);
 }
@@ -97,23 +107,27 @@ select option {
 
 /* Estilo del icono desplegable */
 .select-teams::after {
-    content: '\25BC'; /* Triángulo apuntando hacia abajo como un icono desplegable */
+    content: '\25BC';
+    /* Triángulo apuntando hacia abajo como un icono desplegable */
     font-size: 12px;
     position: absolute;
     top: 50%;
     right: 10px;
     transform: translateY(-50%);
-    pointer-events: none; /* Evita que el icono afecte a la interacción del usuario con el select */
+    pointer-events: none;
+    /* Evita que el icono afecte a la interacción del usuario con el select */
 }
 
 /* Estilo cuando el select está enfocado */
 select:focus {
-    border-color: #2ecc71; /* Nuevo color del borde cuando está enfocado */
+    border-color: #2ecc71;
+    /* Nuevo color del borde cuando está enfocado */
 }
 
 /* Estilo cuando el usuario pasa el ratón sobre el select */
 select:hover {
-    border-color: #e8c30c; /* Nuevo color del borde al pasar el ratón sobre el select */
+    border-color: #e8c30c;
+    /* Nuevo color del borde al pasar el ratón sobre el select */
 }
 
 /* Estilo para la opción seleccionada */
@@ -128,49 +142,135 @@ select option:checked {
         background-color: #3498db;
         color: #fff;
     }
+
     50% {
         background-color: #2ecc71;
         color: #fff;
     }
+
     100% {
         background-color: #3498db;
         color: #fff;
     }
 }
 
+
 /* Aplica la animación a los options al pasar el ratón */
 select option:hover {
     animation: colorChange 1s ease infinite;
 }
 
-/*Animaciones*/
-@keyframes shake-bottom {
-  0%,
-  100% {
-    transform: rotate(0deg);
-    transform-origin: 50% 100%;
-  }
-  10% {
-    transform: rotate(2deg);
-  }
-  20%,
-  40%,
-  60% {
-    transform: rotate(-4deg);
-  }
-  30%,
-  50%,
-  70% {
-    transform: rotate(4deg);
-  }
-  80% {
-    transform: rotate(-2deg);
-  }
-  90% {
-    transform: rotate(2deg);
-  }
+.select-container {
+    flex: 1;
+    /* Toma el espacio disponible */
+    margin-right: 10px;
+    /* Margen entre el select y el botón */
 }
 
+/* botom */
+.button-container {
+  flex: 0; /* No toma espacio adicional */
+}
+button {
+    appearance: button;
+    left: 20px;
+    
+    background-color: #1899D6;
+    border: solid transparent;
+    border-radius: 16px;
+    border-width: 0 0 4px;
+    box-sizing: border-box;
+    color: #FFFFFF;
+    cursor: pointer;
+    display: inline-block;
+    font-size: 15px;
+    font-weight: 700;
+    letter-spacing: .8px;
+    line-height: 20px;
+    margin: 0;
+    outline: none;
+    overflow: visible;
+    padding: 13px 19px;
+    text-align: center;
+    text-transform: uppercase;
+    touch-action: manipulation;
+    transform: translateZ(0);
+    transition: filter .2s;
+    user-select: none;
+    -webkit-user-select: none;
+    vertical-align: middle;
+    white-space: nowrap;
+}
+
+button:after {
+    background-clip: padding-box;
+    background-color: #1CB0F6;
+    border: solid transparent;
+    border-radius: 16px;
+    border-width: 0 0 4px;
+    bottom: -4px;
+    content: "";
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    z-index: -1;
+}
+
+button:main,
+button:focus {
+    user-select: auto;
+}
+
+button:hover:not(:disabled) {
+    filter: brightness(1.1);
+}
+
+button:disabled {
+    cursor: auto;
+}
+
+button:active:after {
+    border-width: 0 0 0px;
+}
+
+button:active {
+    padding-bottom: 10px;
+}
+
+/*Animaciones*/
+@keyframes shake-bottom {
+
+    0%,
+    100% {
+        transform: rotate(0deg);
+        transform-origin: 50% 100%;
+    }
+
+    10% {
+        transform: rotate(2deg);
+    }
+
+    20%,
+    40%,
+    60% {
+        transform: rotate(-4deg);
+    }
+
+    30%,
+    50%,
+    70% {
+        transform: rotate(4deg);
+    }
+
+    80% {
+        transform: rotate(-2deg);
+    }
+
+    90% {
+        transform: rotate(2deg);
+    }
+}
 </style>
 
 
