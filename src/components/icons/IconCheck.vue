954c33f027f193d07affc0c4cbf7d9eca36b2f1c
GowZenitAC <jes_alpuche@outlook.es>
2024-03-02 21:18:58 -0600

    Styling Results View

---
+++ src/components/icons/IconCheck.vue
@@ -0,0 +1,15 @@
+<template lang="">
+   <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-checks" width="30" height="30" viewBox="0 0 24 24" stroke-width="2" stroke="#00b341" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M7 12l5 5l10 -10" />
+  <path d="M2 12l5 5m5 -5l5 -5" />
+</svg>
+</template>
+<script>
+export default {
+    name: 'IconCheck'
+}
+</script>
+<style lang="">
+    
+</style>
--- src/views/ResultsView.vue
+++ src/views/ResultsView.vue
@@ -1,31 +1,141 @@
 <template lang="">
-    <div>
-        <p>Tu puntaje es: {{ puntaje }}</p> <p>Tu tiempo es: {{ tiempo }}</p>
+  <section class="results-test">
+    <div class="titulo">
+        <IconCheck/>
+      <span>¡Felicidades!</span>
+      <p>Completaste el test con éxito.</p>
     </div>
+    <div class="puntuacion">
+      <p>Tu puntuación:</p>
+     <span class="puntaje">{{ puntaje }}pts</span>
+    </div>
+    <div class="tiempo">
+      <p>Tiempo empleado:</p>
+      <span class="tiempo-empleado">{{ tiempo }}  minutos</span>
+    </div>
+</section>
 </template>
 <script>
+import IconCheck from '@/components/icons/IconCheck.vue';
 export default {
-    mounted() {
-        this.getResults()
+  components: {
+    IconCheck
+  },
+  created() {
+    this.getResults();
+  },
+  data() {
+    return {
+      puntaje: 0,
+      tiempo: 0,
+    };
+  },
+  methods: {
+    getResults() {
+      this.puntaje = localStorage.getItem("puntaje");
+      this.tiempo = localStorage.getItem("tiempo");
     },
-    data() {
-        return {
-            puntaje: 0,
-            tiempo: 0,
+  },
+};
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@600&display=swap');
 
-        }
-    },
-    methods: {
-        getResults() {
-           this.puntaje = localStorage.getItem('puntaje')
-            this.tiempo =  localStorage.getItem('tiempo')
-        }
-        
-    }
-
+.results-test {
+  background-color: #145381;
+  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+  grid-template-rows: repeat(auto-fit, minmax(100px, 1fr));
+  gap: 10px;
+  margin: auto;
+  max-width: 900px;
+  width: 100%;
+  animation: slide-in-left 0.8s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+}
+.titulo {
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end:2;
+  display: flex;
+  flex-wrap: wrap;
+}
+.titulo svg {
+  margin: 20px 0 10px 10px;
+  animation: rotate-in-center 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) 1s both; 
+}
+.titulo span {
+  color: white;
+  font-size: 2.5em;
+  margin: 0.1em;
+  font-family: "Outfit", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 600;
+  font-style: normal;
+}
+.titulo p {
+  color: rgba(255, 249, 249, 0.685);
+  font-size: 1.5em;
+  font-family: "Outfit", sans-serif ;
+  margin: 0.2em 0 0.2em 0.5em; 
+}
+.puntuacion {
+  grid-column-start: 1;
+  grid-column-end: 3;
+  display: flex;
+  flex-wrap: wrap;
+  color: white;
+  margin: 0.9em 0 0.9em 1em;
+  font-family: "Outfit", sans-serif;
+}
+.puntuacion p {
+  margin: 1.1em 0.5em 1.1em 0.5em;
+  font-size: 1.5em;
+}
+.puntuacion span {
+  margin: 0.2em 0.1em 0.2em 0.1em;
+  font-size: 2.5em;
+}
+.tiempo {
+  grid-column-start: 3;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 2;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  font-family: 'Outfit', sans-serif;  
+  color: white;
+}
+.tiempo p {
+  font-size: 1.5em;
 
 }
-</script>
-<style lang="">
-    
+.tiempo span {
+  font-size: 2.5em;
+}
+/** Animaciones */
+@keyframes rotate-in-center {
+  0% {
+    transform: rotate(-360deg);
+    opacity: 0;
+  }
+  100% {
+    transform: rotate(0);
+    opacity: 1;
+  }
+}
+@keyframes slide-in-left {
+  0% {
+    transform: translateX(-1000px);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
 </style>
